62209c01b5349c54e403eece5dcf14611c2edf33
mrgw454 <30988211+mrgw454@users.noreply.github.com>
2020-06-03 16:04:39 -0500

    add new update script

---
+++ scripts/updateCoCo-Pi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# script to update CoCo-Pi from github repo
+
+# make sure to be in /home/pi
+cd $HOME
+
+git pull origin master
+
+echo -e
+echo -e
+read -p "Press any key to continue... " -n1 -s
+echo -e
+
