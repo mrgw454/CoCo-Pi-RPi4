ba205de6a2c71baabee9bf65a11ea1abaddcca6c
Ron Klein <ron@domain.org>
2020-06-17 09:09:25 -0500

    update script for downloading latest Fuzix

--- scripts/downloadFuzixNightly.sh
+++ scripts/downloadFuzixNightly.sh
@@ -2,42 +2,49 @@
 
 clear
 
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
+echo -e
+echo -e "This script will download the latest Fuzix nightly or tested build."
+echo -e
+echo -e "Nightly - may or may not work from day to day."
+echo -e "Tested  - will boot and most things should be working."
+echo -e
+echo -e "Warning!  Existing Fuzix installation will be overwritten."
+echo -e
 
-echo -e
-echo -e "This script will download the latest Fuzix nightly build."
-echo -e
-echo -e "The existing file(s) will be overwritten."
-echo -e
-echo -e "Press [CTRL-C] to cancel script or ..."
-echo -e
-read -p "... Press any key to continue." -n1 -s
-echo -e
-echo -e
-echo -e "Downloading latest file..."
-echo -e
-wget -P /media/share1/DW4/GORDON/FUZIX http://fuzix.play-classics.online/fuzix-latest.zip
-
-cd /media/share1/DW4/GORDON/FUZIX
-
-    echo -e
-    unzip -o /media/share1/DW4/GORDON/FUZIX/fuzix-latest.zip
-    echo -e
-    echo -e "Done!"
-    echo -e
-    read -p "Press any key to continue." -n1 -s
-
-rm /media/share1/DW4/GORDON/FUZIX/fuzix-latest.zip
+PS3='Please enter your choice: '
+options=("Fuzix nightly" "Fuzix tested" "Cancel")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Fuzix nightly")
+	    wget -P /media/share1/DW4/GORDON/FUZIX http://fuzix.play-classics.online/fuzix-nightly.zip
+	    cd /media/share1/DW4/GORDON/FUZIX
+	    echo -e
+    	    unzip -o /media/share1/DW4/GORDON/FUZIX/fuzix-nightly.zip
+            rm /media/share1/DW4/GORDON/FUZIX/fuzix-nightly.zip
+            echo -e
+            echo -e "Done!"
+            echo -e
+            read -p "Press any key to continue." -n1 -s
+            break
+	    ;;
+        "Fuzix stable")
+	    wget -P /media/share1/DW4/GORDON/FUZIX http://fuzix.play-classics.online/fuzix-tested.zip
+	    cd /media/share1/DW4/GORDON/FUZIX
+	    echo -e
+    	    unzip -o /media/share1/DW4/GORDON/FUZIX/fuzix-tested.zip
+            rm /media/share1/DW4/GORDON/FUZIX/fuzix-tested.zip
+            echo -e
+            echo -e "Done!"
+            echo -e
+            read -p "Press any key to continue." -n1 -s
+	    break
+            ;;
+        "Cancel")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
 cd $HOME/.mame
-
-
