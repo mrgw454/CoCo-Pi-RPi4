441202a5241f889c322d2322b37085717574a68e
Ron Klein <ron@domain.org>
2020-07-15 12:19:42 -0500

    add new attract mode scripts and updated menu

--- .mame/CoCoPi-menu-Attract.sh
+++ .mame/CoCoPi-menu-Attract.sh
@@ -1,30 +1,30 @@
     clear
     RETVAL=$(whiptail --title "$(cat $HOME/cocopi-release.txt)" \
     --menu "\nPlease select from the following:" 18 70 10 \
-    "1" "Coco 2 MAME     Attract Mode (loop through all)" \
-    "2" "Coco 2 MAME     Attract Mode (random)" \
-    "3" "Coco 2 XRoar    Attract Mode (loop through all)" \
-    "4" "Coco 2 XRoar    Attract Mode (random)" \
-    "5" "Dragon 32 MAME  Attract Mode (loop through all)" \
-    "6" "Dragon 32 MAME  Attract Mode (random)" \
-    "7" "Dragon 32 XRoar Attract Mode (loop through all)" \
+    "1" "Coco 2 MAME     Attract Mode Games (random)" \
+    "2" "Coco 2 MAME     Attract Mode Carts (random)" \
+    "3" "Coco 3 MAME     Attract Mode Games (random)" \
+    "4" "Coco 3 MAME     Attract Mode Carts (random)" \
+    "5" "Coco 2 XRoar    Attract Mode Games (random)" \
+    "6" "Coco 2 XRoar    Attract Mode Carts (random)" \
+    "7" "Dragon 32 MAME  Attract Mode (random)" \
     "8" "Dragon 32 XRoar Attract Mode (random)" \
     "9" "Coco 3 MAME     Image Slideshow" \
     "10" "Coco 3 MAME     CoCoTracker Demo" \
-    "11" "MAME            Play Donkey Kong Hack" \
+    "11" "MAME            Play Donkey Kong Hacks" \
     "12" "Return to Main Menu" \
     3>&1 1>&2 2>&3)
 
     # Below you can enter the corresponding commands
 
     case $RETVAL in
-        1) attract-mode.sh && CoCoPi-menu-Attract.sh;;
-        2) attract-mode-random.sh && CoCoPi-menu-Attract.sh;;
-        3) attract-mode-xroar.sh && CoCoPi-menu-Attract.sh;;
-        4) attract-mode-random-xroar.sh && CoCoPi-menu-Attract.sh;;
-        5) attract-mode-dragon.sh && CoCoPi-menu-Attract.sh;;
-        6) attract-mode-random-dragon.sh && CoCoPi-menu-Attract.sh;;
-        7) attract-mode-dragon-xroar.sh && CoCoPi-menu-Attract.sh;;
+        1) attract-mode-random-coco2.sh /media/share1/coco/games/coco2 && CoCoPi-menu-Attract.sh;;
+        2) attract-mode-random-coco2.sh /media/share1/carts && CoCoPi-menu-Attract.sh;;
+        3) attract-mode-random-coco3.sh /media/share1/coco/games/coco3 && CoCoPi-menu-Attract.sh;;
+        4) attract-mode-random-coco3.sh /media/share1/carts && CoCoPi-menu-Attract.sh;;
+        5) attract-mode-random-coco2-xroar.sh /media/share1/coco/games/coco2 && CoCoPi-menu-Attract.sh;;
+        6) attract-mode-random-coco2-xroar.sh /media/share1/carts && CoCoPi-menu-Attract.sh;;
+        7) attract-mode-random-dragon.sh && CoCoPi-menu-Attract.sh;;
         8) attract-mode-random-dragon-xroar.sh && CoCoPi-menu-Attract.sh;;
         9) attract-mode-gallery.sh && CoCoPi-menu-Attract.sh;;
        10) attract-mode-ORCH90.sh && CoCoPi-menu-Attract.sh;;
@@ -32,3 +32,4 @@
        12) menu;;
         *) echo "Quitting...";;
     esac
+
