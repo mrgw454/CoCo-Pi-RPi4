e2d1ca8b09f7e5c7f10c2dd36094843b373f7648
Ron Klein <ron@domain.org>
2020-07-21 11:20:29 -0500

    add new support for MC-10 MCX-128

--- .mame/CoCoPi-menu-MC10.sh
+++ .mame/CoCoPi-menu-MC10.sh
@@ -3,7 +3,7 @@
     --menu "\nPlease select from the following:" 18 65 10 \
     "1" "TRS-80 Micro Color Computer   4K" \
     "2" "TRS-80 Micro Color Computer  20K" \
-    "3" "TRS-80 Micro Color Computer 128K (MCX BASIC)" \
+    "3" "TRS-80 Micro Color Computer 128K (MCX BASIC) & pyDW" \
     "4" "Return to Main Menu" \
     3>&1 1>&2 2>&3)
 
@@ -12,7 +12,7 @@
     case $RETVAL in
         1) mc-10-4k.sh;;
         2) mc-10-20k.sh;;
-        3) mc-10-128k.sh;;
+        3) mc-10-MCX128-pyDW.sh;;
         4) menu;;
         *) echo "Quitting...";;
     esac
